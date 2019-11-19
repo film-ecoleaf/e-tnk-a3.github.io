6400fea994a034e7a7617f042a6985183d8e1156
e-tnk-a3 <eisuke.20140812@gmail.com>
2019-11-19 09:04:52 +0900

    TOPページアニメーション実装
    フェードイン/フェードアウトの実装
    文言微調整

---
+++ js/animation.js
@@ -0,0 +1,23 @@
+$(function(){
+    // Scriptが機能してない場合のコンテンツ非表示を防ぐためにこのタイミングで初期設定
+    $('.fade-in').css("opacity","0");
+    $('.fade-in').css("margin-top","30px");
+    // スクロール監視 parallaxの仕様上、window.scrollだと効かないのでbody.scroll
+    $("body").scroll(function(){
+        $('.fade-in').each(function(){
+            var imgPos = $(this).offset().top;
+            var scroll = $("body").scrollTop();
+            var windowHeight = $("body").height();
+
+            // スクロール位置がフェードインコンテンツの高さにあるか
+            if (scroll > imgPos - windowHeight + windowHeight){
+                $(this).css("opacity","1");
+                $(this).css("margin-top","0");
+            } else {
+                // フェードアウトする場合記述
+                $(this).css("opacity","0");
+                $(this).css("margin-top","30px");
+            }
+        })
+    })
+})    
