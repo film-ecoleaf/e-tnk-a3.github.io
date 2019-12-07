9cdd588757baf67bdaba314ccecaa0deb0b0c501
e-tnk-a3 <eisuke.20140812@gmail.com>
2019-12-07 15:16:12 +0900

    Revert "NodeJSを使用したpractice12の追加"
    This reverts commit bb3531c519cdc3fe1bd30ec4fb16a817bcfde151.

--- practice12/js/index.js
+++
@@ -1,9 +0,0 @@
-import Highway from '@dogstudio/highway';
-import Fade from './transition';
-import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from 'constants';
-
-const H = new Highway.Core({
-    transitions: {
-        default: Fade
-    }
-});
