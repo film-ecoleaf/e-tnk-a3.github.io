5bdef19459c84402357eea099bcb17aeb1494406
e-tnk-a3 <eisuke.20140812@gmail.com>
2019-12-15 19:54:14 +0900

    アカウント関連のレイアウト作成
    ・ログイン
    ・アカウント登録
    ・アカウント変更
    上記3フォームと
    ・フッター
    ・スクロールアニメーション
    上記2ポイントの作成

--- practice15/js/animation.js
+++ practice15/js/animation.js
@@ -1,5 +1,18 @@
 $(function () {
-    $(window).scroll(function () {
+
+    $(window).scroll(ScrollAnimation)
+    $('.menu-btn').on('click', OpenMenu);
+    $('.high-light').on('click', OpenModal);
+    $('.account-btn').on('click', OpenModal);
+    $('.login-btn').on('click', OpenModal);
+    $('.edit-btn').on('click', OpenModal);
+    $('main').on('click', CloseMenu);
+    $('.close-modal-btn, .modal').on('click', CloseModal);
+    $('.form').on('click', function () {
+        event.stopPropagation();
+    })
+
+    function ScrollAnimation() {
         $('.cook-name').each(function () {
             var imgPos = $(this).offset().top;
             var scroll = $(window).scrollTop();
@@ -12,5 +25,24 @@ $(function () {
                 // フェードアウトする場合記述
             }
         })
-    })
+    }
+    function OpenMenu() {
+        $('.nav-wrapper').toggleClass('on');
+    }
+    function CloseMenu() {
+        $('.nav-wrapper').removeClass('on');
+    }
+    function OpenModal() {
+        CloseModal();
+        $('.modal').addClass('on');
+        $($(this).data('form-name')).addClass('on');
+        $('html').css('overflow-y', 'hidden');
+    }
+    function CloseModal() {
+        $('.modal').removeClass('on');
+        $('.modal').find('.on').each(function () {
+            $(this).removeClass('on');
+        });
+        $('html').css('overflow-y', 'auto');
+    }
 })
